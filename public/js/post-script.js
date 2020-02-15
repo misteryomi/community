a9167f3ca125287f28fec375c4154fd68daf811b
Oluwayomi Omotoso <oomotoso@primeracredit.local>
2020-02-15 11:11:27 +0100

    local

--- public/js/post-script.js
+++ public/js/post-script.js
@@ -1,1 +1,219 @@
-!function(t){var e={};function a(o){if(e[o])return e[o].exports;var r=e[o]={i:o,l:!1,exports:{}};return t[o].call(r.exports,r,r.exports,a),r.l=!0,r.exports}a.m=t,a.c=e,a.d=function(t,e,o){a.o(t,e)||Object.defineProperty(t,e,{enumerable:!0,get:o})},a.r=function(t){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},a.t=function(t,e){if(1&e&&(t=a(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var o=Object.create(null);if(a.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var r in t)a.d(o,r,function(e){return t[e]}.bind(null,r));return o},a.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return a.d(e,"a",e),e},a.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},a.p="/",a(a.s=2)}([,,function(t,e,a){t.exports=a(3)},function(t,e){$(document).ready((function(){var t,e,a;$("#submit-comment").click((function(t){t.preventDefault();var e=editor.getData();return $("input[name=comment]").val(e),$("#comment-form").submit(),!1})),$("a > .like").click((function(t){return t.preventDefault(),loggedIn?($(this).shake(),$(this).hasClass("liked")?($(this).removeClass("liked fa-thumbs-up").addClass("fa-thumbs-o-up"),$(".likes-count").text(parseInt($(".likes-count").text())-1),$.post(slug+"/unlike")):($(this).removeClass("fa-thumbs-o-up").addClass("liked fa-thumbs-up"),$(".likes-count").text(parseInt($(".likes-count").text())+1),$.post(slug+"/like"))):$("#auth-modal").modal("show"),!1})),$("a.comment-like").click((function(t){t.preventDefault();var e=$(this).find("i.fa"),a=$(this).attr("data-id"),o=$(".comment-likes-count[data-id="+a+"]");return $(this).shake(),$(this).hasClass("liked")?($(e).removeClass("liked fa-thumbs-up").addClass("fa-thumbs-o-up"),o.text(parseInt(o.text())-1),$.post("comment/".concat(a,"/unlike"))):($(e).removeClass("fa-thumbs-o-up").addClass("liked fa-thumbs-up"),o.text(parseInt(o.text())+1),$.post("comment/".concat(a,"/unlike"))),!1})),$("a.bookmark").click((function(t){return t.preventDefault(),$(this).hasClass("bookmarked")?($(this).removeAttr("data-original-title").attr({"data-original-title":"Remove from Saved"}),$("a.bookmark > i").addClass("fa-bookmark-o").removeClass("fa-bookmark"),$(this).removeClass("bookmarked"),$('[data-toggle="tooltip"]').tooltip("hide"),$.post(slug+"/remove-bookmark")):($(this).removeAttr("data-original-title").attr({"data-original-title":"Save for later"}),$(this).addClass("bookmarked"),$("a.bookmark > i").addClass("fa-bookmark").removeClass("fa-bookmark-o"),$('[data-toggle="tooltip"]').tooltip("hide"),$.post(slug+"/bookmark")),!1})),jQuery.fn.shake=function(){for(var t=1;t<=3;t++)$(this).animate({marginLeft:2},10).animate({marginLeft:-2},50);return this},a='<iframe width="560" height="315" src="//www.youtube.com/embed/'+(t=$("figure.media oembed").attr("url"),(e=t.match(/^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/))&&11==e[2].length?e[2]:"error")+'" frameborder="0" allowfullscreen></iframe>',$("figure.media").html(a)}))}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./resources/assets/js/post-script.js":
+/*!********************************************!*\
+  !*** ./resources/assets/js/post-script.js ***!
+  \********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+$(document).ready(function () {
+  $('#submit-comment').click(function (e) {
+    e.preventDefault();
+    var comment = editor.getData();
+    $("input[name=comment]").val(comment);
+    $('#comment-form').submit();
+    return false;
+  });
+  $("a > .like").click(function (e) {
+    e.preventDefault();
+
+    if (!loggedIn) {
+      $('#auth-modal').modal('show');
+    } else {
+      $(this).shake();
+
+      if ($(this).hasClass('liked')) {
+        $(this).removeClass('liked fa-thumbs-up').addClass('fa-thumbs-o-up');
+        $('.likes-count').text(parseInt($('.likes-count').text()) - 1);
+        $.post(slug + "/unlike");
+      } else {
+        $(this).removeClass('fa-thumbs-o-up').addClass('liked fa-thumbs-up');
+        $('.likes-count').text(parseInt($('.likes-count').text()) + 1);
+        $.post(slug + "/like");
+      }
+    }
+
+    return false;
+  });
+  $("a.comment-like").click(function (e) {
+    e.preventDefault();
+    var el = $(this).find('i.fa');
+    var id = $(this).attr('data-id');
+    var likesEl = $(".comment-likes-count[data-id=" + id + "]");
+    $(this).shake();
+
+    if ($(this).hasClass('liked')) {
+      $(el).removeClass('liked fa-thumbs-up').addClass('fa-thumbs-o-up');
+      likesEl.text(parseInt(likesEl.text()) - 1);
+      $.post("comment/".concat(id, "/unlike"));
+    } else {
+      $(el).removeClass('fa-thumbs-o-up').addClass('liked fa-thumbs-up');
+      likesEl.text(parseInt(likesEl.text()) + 1);
+      $.post("comment/".concat(id, "/unlike"));
+    }
+
+    return false;
+  });
+  $("a.bookmark").click(function (e) {
+    e.preventDefault();
+
+    if ($(this).hasClass('bookmarked')) {
+      $(this).removeAttr('data-original-title').attr({
+        // 'title' : 'Remove from Saved',
+        'data-original-title': 'Remove from Saved'
+      });
+      $('a.bookmark > i').addClass('fa-bookmark-o').removeClass('fa-bookmark');
+      $(this).removeClass('bookmarked');
+      $('[data-toggle="tooltip"]').tooltip("hide");
+      $.post(slug + "/remove-bookmark");
+    } else {
+      $(this).removeAttr('data-original-title').attr({
+        // 'title' : 'Save for later',
+        'data-original-title': 'Save for later'
+      });
+      $(this).addClass('bookmarked');
+      $('a.bookmark > i').addClass('fa-bookmark').removeClass('fa-bookmark-o');
+      $('[data-toggle="tooltip"]').tooltip("hide");
+      $.post(slug + "/bookmark");
+    }
+
+    return false;
+  });
+
+  jQuery.fn.shake = function () {
+    // this.each(function(i) {
+    for (var x = 1; x <= 3; x++) {
+      $(this).animate({
+        marginLeft: 2
+      }, 10).animate({
+        marginLeft: -2
+      }, 50);
+    } // });
+
+
+    return this;
+  };
+
+  function getId(url) {
+    var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    var match = url.match(regExp);
+
+    if (match && match[2].length == 11) {
+      return match[2];
+    } else {
+      return 'error';
+    }
+  }
+
+  function replaceOEmbed() {
+    var url = $('figure.media oembed').attr('url');
+    var videoId = getId(url);
+    var iframeMarkup = '<iframe width="560" height="315" src="//www.youtube.com/embed/' + videoId + '" frameborder="0" allowfullscreen></iframe>';
+    $('figure.media').html(iframeMarkup);
+  }
+
+  replaceOEmbed();
+});
+
+/***/ }),
+
+/***/ 1:
+/*!**************************************************!*\
+  !*** multi ./resources/assets/js/post-script.js ***!
+  \**************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! C:\laragon\www\yarnable\resources\assets\js\post-script.js */"./resources/assets/js/post-script.js");
+
+
+/***/ })
+
+/******/ });
