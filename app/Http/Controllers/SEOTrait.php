dda73e0a08261cef5f3a5f29e58a0d82d7ee7fc5
unknown <yomiomotoso@gmail.com>
2020-12-16 11:11:58 +0100

    SEO fixes

--- app/Http/Controllers/SEOTrait.php
+++ app/Http/Controllers/SEOTrait.php
@@ -7,7 +7,7 @@ use Artesaos\SEOTools\Facades\SEOTools;
 trait SEOTrait {
 
 
-    public function setSEO($title, $description, $url, $propertyType = 'article', $site='@411ng_',  $canonical = null, $image = null) {
+    public function setSEO($title, $description, $url, $image = null, $propertyType = 'article', $site='@jaracentralHQ',  $canonical = null ) {
         SEOTools::setTitle($title);
         SEOTools::setDescription($description);
         SEOTools::opengraph()->setUrl($url);
@@ -16,6 +16,10 @@ trait SEOTrait {
         SEOTools::twitter()->setSite($site);
         if($image) {
             SEOTools::jsonLd()->addImage($image);
+            SEOTools::opengraph()->addImage($image);
+            SEOTools::twitter()->addImage($image);
+            // OpenGraph::addImage($post->cover->url);
+
         }
     }
 }
