52d9470ff3161bfbfe38399ee8f236ea39a5f075
unknown <yomiomotoso@gmail.com>
2020-12-16 15:22:30 +0100

    x

--- resources/views/layouts/posts/show.blade.php
+++ resources/views/layouts/posts/show.blade.php
@@ -199,6 +199,25 @@
 
 
     })
+
+    //Fix oembedd 
+    $('figure.media').ready(function() {
+        console.log('ready')
+        let ytUrlEl = $(this).find('oembed');
+        let ytID = youtube_parser(ytUrlEl.attr('url'));
+
+        console.log({ytID});
+
+        $(this).html(`<iframe width="100%" height="450" src="http://www.youtube.com/embed/${ytID}" frameborder="0" allowfullscreen></iframe>`)
+
+    });
+
+    function youtube_parser(url){
+        var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+        var match = url.match(regExp);
+        return (match&&match[7].length==11)? match[7] : false;
+    }
+    
 </script>
 <!-- <script src="{{asset('js/post-script.js')}}" ></script> -->
 @endsection
